42eec0974671eb1da58c289a3de49c67b0b678fd
New Panuwat <new_sbh@me.com>
2022-10-28 14:16:15 +0700

    add / request serverside in /app

--- app/page.js
+++ app/page.js
@@ -1,4 +1,15 @@
 // This file maps to the index route (/)
-export default function Page() {
-  return <h1>Hello, Next.js! 13</h1>;
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  return <span>{JSON.stringify(data, null, 1)}</span>;
 }
